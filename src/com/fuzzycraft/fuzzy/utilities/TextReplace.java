88c382bc0d6b502fbca0c81623b6debfbf7b3d90
FuzzyStatic <af732@drexel.edu>
2015-07-07 18:53:40 -0400

    Colors

--- src/com/fuzzycraft/fuzzy/utilities/TextReplace.java
+++ src/com/fuzzycraft/fuzzy/utilities/TextReplace.java
@@ -1,5 +1,14 @@
 package com.fuzzycraft.fuzzy.utilities;
 
+/**
+ * 
+ * @author FuzzyStatic (fuzzy@fuzzycraft.com)
+ *
+ */
+
 public class TextReplace {
 
+	public static String colors(String string) {
+		return string.replaceAll("&(?<!&&)(?=[0-9a-fA-F])", "\u00A7").replace("&&","&");
+	}
 }
